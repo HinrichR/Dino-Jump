f4cb867eba2e4d2cd508a15848ccde90e0941960
HinrichR <hinne.spinne@gmail.com>
2025-06-05 10:34:19 +0200

    fix tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,6 @@
 // jest.setup.js
 const { createCanvas } = require('canvas');
 
-Object.defineProperty(global, 'HTMLCanvasElement', {
-  value: function () {},
-  writable: true,
-});
-
 Object.defineProperty(global.HTMLCanvasElement.prototype, 'getContext', {
   value: function (type) {
     return createCanvas(800, 600).getContext(type);
